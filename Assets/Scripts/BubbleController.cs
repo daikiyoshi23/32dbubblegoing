bae4812e4cc9099c9c47ebbcef370edba2e9d368
DaiDaiPc\Daiki <21131157dy@tama.ac.jp>
2018-07-23 20:19:05 +0900

    キャラクターアニメーションの調整まで

--- Assets/Scripts/BubbleController.cs
+++ Assets/Scripts/BubbleController.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class BubbleController : MonoBehaviour {
 
     Rigidbody2D rigidbody2D;
+    Animator animator;
+    float angle;
 
     public float maxHeight;
     public float jumpVerocity;
+    public float movingVelocityX;
+    public GameObject bubbleSprite;
+    bool isGround = false;
 
 	// Use this for initialization
 	void Start () {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = bubbleSprite.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -20,9 +26,21 @@ public class BubbleController : MonoBehaviour {
         {
             Jump();
         }
+
+        ReflectAngle();
+        animator.SetBool("isGround", isGround);
+
+        
 	}
+
     void Jump()
     {
         rigidbody2D.velocity = new Vector2(0.0f, jumpVerocity);
+    }
+    void ReflectAngle()
+    {
+        float targetAngle = Mathf.Atan2(rigidbody2D.velocity.y, movingVelocityX) * Mathf.Rad2Deg;
+        angle = Mathf.Lerp(angle, targetAngle, Time.deltaTime * 10.0f);
+        bubbleSprite.transform.localRotation = Quaternion.Euler(0.0f, 0.0f, angle);
     }
 }
