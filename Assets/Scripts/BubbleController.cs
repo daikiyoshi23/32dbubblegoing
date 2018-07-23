9b15a4530e3ab5ebb133b5f00b8d56c201df2c84
DaiDaiPc\Daiki <21131157dy@tama.ac.jp>
2018-07-23 20:39:25 +0900

    キャラクター機能の追加まで

--- Assets/Scripts/BubbleController.cs
+++ Assets/Scripts/BubbleController.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class BubbleController : MonoBehaviour {
 
-    Rigidbody2D rigidbody2D;
+    Rigidbody2D rigidbody2d;
     Animator animator;
     float angle;
+    bool isDead = false;
 
     public float maxHeight;
     public float jumpVerocity;
@@ -14,9 +15,14 @@ public class BubbleController : MonoBehaviour {
     public GameObject bubbleSprite;
     bool isGround = false;
 
+    public bool IsDead()
+    {
+        return isDead;
+    } 
+
 	// Use this for initialization
 	void Start () {
-        rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2d = GetComponent<Rigidbody2D>();
         animator = bubbleSprite.GetComponent<Animator>();
 	}
 	
@@ -35,12 +41,46 @@ public class BubbleController : MonoBehaviour {
 
     void Jump()
     {
-        rigidbody2D.velocity = new Vector2(0.0f, jumpVerocity);
+        if (isDead)
+        {
+            return;//ジャンプメソッドを抜ける   
+        }
+        isGround = false;
+
+        rigidbody2d.velocity = new Vector2(0.0f, jumpVerocity);
     }
     void ReflectAngle()
     {
-        float targetAngle = Mathf.Atan2(rigidbody2D.velocity.y, movingVelocityX) * Mathf.Rad2Deg;
+        float targetAngle;
+
+        if (isDead)
+        {
+            targetAngle = 180.0f;
+        }
+        else
+        {
+            targetAngle = Mathf.Atan2(rigidbody2d.velocity.y, movingVelocityX) * Mathf.Rad2Deg;
+        }
+
         angle = Mathf.Lerp(angle, targetAngle, Time.deltaTime * 10.0f);
         bubbleSprite.transform.localRotation = Quaternion.Euler(0.0f, 0.0f, angle);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Block")
+        {
+            if (isDead)
+            {
+                return;
+            }
+            animator.enabled = false;
+
+            isDead = true;
+        }
+        else
+        {
+            isGround = true;
+        }
     }
 }
